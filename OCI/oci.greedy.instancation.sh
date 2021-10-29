a4f8e9194099bd3b8b0666713602b3730f2ac885
furplag <furplag@furplag.jp>
2021-10-29 19:15:29 +0900

    Python 3 RuntimeError

--- OCI/oci.greedy.instancation.sh
+++ OCI/oci.greedy.instancation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ue -o pipefail
-export LC_ALL=C
+export LC_ALL=${LC_ALL:-C.utf8}
+export LC_LANG=${LC_LANG:-C.utf8}
 
 # oci.greedy.instancation.sh
 # https://github.com/furplag/archive/OCI
