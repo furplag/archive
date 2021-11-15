42d2aa423aac7e2bc7b0d0644d18f5c7755b15de
furplag <furplag@users.noreply.github.com>
2021-11-15 14:40:58 +0900

    Update minecraft.install.sh

--- Minecraft/minecraft.install.sh
+++ Minecraft/minecraft.install.sh
@@ -302,6 +302,13 @@ Type=forking
 User=${_user}
 Group=${_group}
 
+PrivateUsers=true
+ProtectSystem=full
+ProtectHome=true
+ProtectKernelTunables=true
+ProtectKernelModules=true
+ProtectControlGroups=true
+
 WorkingDirectory=${_basedir}/%i
 ExecStart=${_basedir}/%i/startup
 ExecStop=/bin/bash -c "waits=$(_i=${config[shutdown_delay_seconds]}; if [[ $((${_i:-0})) -lt 0 ]]; then echo '0'; else echo "${_i:-0}"; fi); while [[ \$((waits)) -gt 0 ]]; do screen -p 0 -S %i -X eval \"stuff 'say the world freezing, at least in \$waits sec ...'\\015\"; waits=\$((waits - 5)); sleep 5; done"
