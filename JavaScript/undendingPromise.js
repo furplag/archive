373fe93ef243c72dfea6a5234128a728cb10f41a
furplag <furplag@users.noreply.github.com>
2023-01-11 19:02:04 +0900

    Update undendingPromise.js

--- JavaScript/undendingPromise.js
+++ JavaScript/undendingPromise.js
@@ -1,20 +1,17 @@
 /**
- * Unbending Promise
- * useful for retrying an operation that may sometimes fail for transient reasons, and you want 
- * to implement a mechanism to handle these failures automatically .
+ * Unbending Promise is useful for retrying an operation that may sometimes fail for transient reasons .
+ * it can be used to stop the retry loop whenever you want and exit the loop gracefully before the limit is reached . 
+ * this can be achieved by setting the 'abort' parameter to true .
  *
- * you can set the abort parameter to true to stop the infinite loop whenever you want, in order to exit the loop 
- * gracefully without waiting for the limit to be reached. And also avoid infinite loop .
- * 
  * @function unbendingPromise
- * @param {Function} fn - a function to retry . it should return a promise that resolves to a boolean indicating success or failure .
- * @param {string} [beacon] - a string to identify the process being retried .
- * @param {number} [limit=100] - the number of times to retry the function .
- * @param {number} [delay=100] - the delay in milliseconds between retries .
- * @param {boolean} [abort=false] - the delay in milliseconds between retries .
+ * @param {Function} fn - a function to retry, it should return a promise that resolves to a boolean indicating success or failure .
+ * @param {string} [beacon] - an optional identifier to identify the process being retried
+ * @param {number} [limit=100] - the maximum number of attempts to retry the function before it is rejected
+ * @param {number} [delay=100] - the delay in milliseconds between retries
+ * @param {boolean} [abort=false] - a flag that when set to true, will cause the promise to reject immediately .
  * 
- * @returns {Promise} - a promise that will resolve if the provided function resolve to true, otherwise it will retry with a delay .
- * the promise will be rejected if the limit is reached or the abort flag is true .
+ * @returns {Promise} - a promise that will resolve when the provided function returns true . the promise will retry with a delay 
+ * if the provided function returns false, and it will be rejected if the limit is reached or the abort flag is true .
  */
 function unbendingPromise(fn = () => Promise.resolve(true), beacon, limit = 100, delay = 100, abort = false) {
   let counter = limit;
